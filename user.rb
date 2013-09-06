87a602c4423ae7e5d761ebb99743fcdb145a7504
MakerSquare Student <students@themakersquare.com>
2013-09-06 11:12:07 -0500

    Changed name to attr_reader so that it cannot be changed. Added a check_out method to allows users to check out books that are available and ensures that  the user has no mroe than 2 books out at a time.

--- user.rb
+++ user.rb
@@ -11,7 +11,7 @@
   # Users are also greeted with open arms to the MakerSquare library!
 
 class User < Book 
-  attr_accessor :name
+  attr_reader :name
 
   def initialize(name="Drew")
   	@name = name
@@ -19,9 +19,28 @@ class User < Book
     @books_out = 0
   end
 
+  # Created a check_out method to allow users to check books out.
+  # The method checks to make sure that the user does not have more that 2 books checked out.
+  # It then changes the status of any book that is checked out to "Checked Out"
+  # It also prevents users from checking out a book that is checked out.
+  # 
+  # Example
+  #
+  #    user1.check_out(book1)
+  #    book1.status = "Check Out"
 
-  
- 
+  def check_out(book)
+    if @books_out > 2 then
+      puts "You have too many books checked out!"
+      puts "Please return a book before checking out another one."
+    elsif book.status == "Checked Out"
+      puts "Sorry, this book is already checked out!"
+    else
+      puts "You checked out #{book.title}. Please bring it back in a week."
+      @books_out = @books_out + 1
+      book.status = "Checked Out" 
+    end
+  end
 
 
 end
