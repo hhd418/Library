adfc80a9aa850cc9cac35af18ab9b2206d779ca3
MakerSquare Student <students@themakersquare.com>
2013-09-06 13:50:27 -0500

    Updated/added a Library class that creates a library and allows it have users and books through the add_book method and the add_user method.

--- library.rb
+++ library.rb
@@ -1,14 +1,43 @@
+# Created a Library class.
+# Each library is initialized and given a default name of MakerSquare.
+# Each library is also assigned a books array to keep track of the books that is has.
+# Each library is also assigned a users array to keep track of its memebrs.
+
 class Library < User 
 
+  attr_accessor :books, :users
 
   def initialize(lib="MakerSquare")
   	@lib = lib
-    @book = 
+    @books = []
+    @users = []
   end
 
-  # The add_book method allows users to add books to the MakerSquare library.
+  # The add_book method allows users to add books to the library.
+  # Books are added to the books array
+  #
+  # Example
+  #
+  #    lib1.add_book(book1)
+  #    lib2.add_book(book2)
+  #    @books = [book1, book2]
 
- 
+  def add_book(book)
+    @books << book
+  end
+
+  # The add_book method allows users to add books to the library.
+  # Books are added to the books array
+  #
+  # Example
+  #
+  #    lib1.add_user(book1)
+  #    lib2.add_user(book2)
+  #    @users = [book1, book2]
+
+  def add_user(user)
+    @users << user
+  end
 
 
 end
