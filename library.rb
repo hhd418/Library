abf8cde7c909e01f3024009bbe93d332b5dd9782
MakerSquare Student <students@themakersquare.com>
2013-09-06 11:58:38 -0500

    Changed library back to a basic class. Next step will be to add an index to be able to list books.

--- library.rb
+++ library.rb
@@ -1,35 +1,26 @@
-class User
+class Library < User 
 
-  # Created a user class and initialized it with a name argument.
-  #
-  # Examples
-  #
-  #   user1 = User.new('Mike')
-  #   Will give user1 the name of Mike.
-  #   If no name is entered the user will have a default name of Drew!
-  #
-  # Users are also greeted with open arms to the MakerSquare library!
-  def initialize(name="Drew")
-  	@name = name
-    puts "Welcome #{@name} to the MakerSquare library!"
+
+  def initialize(lib="MakerSquare")
+  	@lib = lib
+    @book = 
   end
 
-  def add_book(author,title,description)
-  end
+  # The add_book method allows users to add books to the MakerSquare library.
 
-  def book_info
-  end
+ 
 
-  def check_out()
-  end
-
-  def overdue?(name)
-  end
-
-  def check_in(name)
-  end
-
-  def overdue_list
-  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
