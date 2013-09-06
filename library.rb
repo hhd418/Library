61679611a9fc662c462a6618a558ebf316c6f4e7
MakerSquare Student <students@themakersquare.com>
2013-09-06 15:38:54 -0500

    Created the check_out method to allow users to check out books.

--- library.rb
+++ library.rb
@@ -53,6 +53,41 @@ class Library
     end
   end
 
+  # Created a check_out method to allow users to check books out.
+  # The method first makes sure that the user_standing is not bad. If it is that means the user has an overdue book and cannot check out anymroe books.
+  # The method checks to make sure that the user does not have more that 2 books checked out.
+  # It then changes the status of any book that is checked out to "Checked Out"
+  # It also prevents users from checking out a book that is checked out.
+  # The method assigns a due date as well 
+  # 
+  #
+  # Example
+  #
+  #    user1.check_out(book1)
+  #    book1.status = "Check Out"
+  #    book1.due_date is now equal to one week from the checkout
+
+  def check_out(book, user)
+   # if @user_standing == "Bad"
+    #  puts "You are not aloud to check out books because you have an overdue book."
+    # else
+      if user.books_out > 2 then
+        puts "You have too many books checked out!"
+        puts "Please return a book before checking out another one."
+      elsif book.status == "Checked Out"
+        puts "Sorry, this book is already checked out!"
+      else
+        puts "You checked out #{book.title}. Please bring it back in a week."
+        user.books_out = user.books_out + 1
+        book.status = "Checked Out"
+        book.owner = user.name
+        book.due_date = Time.now + (7*24*60*60)
+        puts "Your book is due #{book.due_date}!"
+      end
+    #end
+  end
+
+
   # The books_overdue? method will iterate through the books array to find books tht Overdue.
   # Books that are overdue are then outputed
   #
@@ -80,7 +115,7 @@ class Library
   def books_out
     @books.each do |book|
       if book.status == "Checked Out"
-          puts "#{book.title} was check out by #{book.owner} and is due #{book.due_date}."
+          puts "#{book.title} was checked out by #{book.owner} and is due #{book.due_date}."
       end
     end
   end
