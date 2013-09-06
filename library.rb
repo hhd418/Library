4899732be406c49c6c897694b1f674f5010fda7d
MakerSquare Student <students@themakersquare.com>
2013-09-06 14:34:09 -0500

    Added a way to print out the tittle of the books in the library and the created a books_overdue? method to find out which books are overdue."

--- library.rb
+++ library.rb
@@ -3,7 +3,7 @@
 # Each library is also assigned a books array to keep track of the books that is has.
 # Each library is also assigned a users array to keep track of its memebrs.
 
-class Library < User 
+class Library  
 
   attr_accessor :books, :users
 
@@ -31,14 +31,42 @@ class Library < User
   #
   # Example
   #
-  #    lib1.add_user(book1)
-  #    lib2.add_user(book2)
-  #    @users = [book1, book2]
+  #    lib1.add_user(user1)
+  #    lib2.add_user(user2)
+  #    @users = [user1, user2]
 
   def add_user(user)
     @users << user
   end
 
+  # The titles method with print all of the title the book currently has.
+  #
+  # Example
+  #
+  #   lib1.titles
+  #   It will iterate through the books array and print out the book titles.
+  
+  def titles
+    @books.each do |book|
+      puts "We have #{book.title}."
+    end
+  end
+
+  # The books_overdue? method will iterate through the books array to find books tht Overdue.
+  # Books that are overdue are then outputed
+  #
+  # Example
+  #
+  #   lib1.books_overdue?
+  #   will print all books that have book
+
+  def books_overdue?
+    @books.each do |book|
+      if book.status = "Overdue"
+        puts "#{book.title} is Overdue!"
+      end
+    end
+  end
 
 end
 
